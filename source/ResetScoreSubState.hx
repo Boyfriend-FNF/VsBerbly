b33520fe59b5703ec7a0d0b6f7301802a4a8d89e
e <96701297+idklool69@users.noreply.github.com>
2022-03-05 19:36:47 -0300

    Update ResetScoreSubState.hx

--- source/ResetScoreSubState.hx
+++ source/ResetScoreSubState.hx
@@ -68,6 +68,9 @@ class ResetScoreSubState extends MusicBeatSubstate
 		noText.x += 200;
 		add(noText);
 		updateOptions();
+
+                addVirtualPad(LEFT_RIGHT, A_B);
+                addPadCamera();
 	}
 
 	override function update(elapsed:Float)
@@ -88,7 +91,11 @@ class ResetScoreSubState extends MusicBeatSubstate
 		}
 		if(controls.BACK) {
 			FlxG.sound.play(Paths.sound('cancelMenu'), 1);
-			close();
+			#if android
+                        MusicBeatState.resetState();
+                        #else
+                        close();
+                        #end
 		} else if(controls.ACCEPT) {
 			if(onYes) {
 				if(week == -1) {
@@ -98,7 +105,11 @@ class ResetScoreSubState extends MusicBeatSubstate
 				}
 			}
 			FlxG.sound.play(Paths.sound('cancelMenu'), 1);
-			close();
+			#if android
+                        MusicBeatState.resetState();
+                        #else
+                        close();
+                        #end
 		}
 		super.update(elapsed);
 	}
