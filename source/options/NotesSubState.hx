94faa5dfa41ad202f7ef8f1810667587f03a3b38
quenose <101024734+quenose@users.noreply.github.com>
2022-03-05 21:53:13 -0300

    Update NotesSubState.hx

--- source/options/NotesSubState.hx
+++ source/options/NotesSubState.hx
@@ -91,6 +91,8 @@ class NotesSubState extends MusicBeatSubstate
 		add(hsbText);
 
 		changeSelection();
+
+                addVirtualPad(FULL, A_B_C);
 	}
 
 	var changingNote:Bool = false;
@@ -175,7 +177,11 @@ class NotesSubState extends MusicBeatSubstate
 
 		if (controls.BACK || (changingNote && controls.ACCEPT)) {
 			if(!changingNote) {
-				close();
+				#if android
+			MusicBeatState.resetState();
+			#else
+			close();
+			#end
 			} else {
 				changeSelection();
 			}
