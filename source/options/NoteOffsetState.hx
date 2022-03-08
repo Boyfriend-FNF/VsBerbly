6805ae3f67ec573addb852e63e252c94c4662708
Boyfriend-FNF <83258333+Boyfriend-FNF@users.noreply.github.com>
2022-03-08 19:53:13 -0300

    Update NoteOffsetState.hx

--- source/options/NoteOffsetState.hx
+++ source/options/NoteOffsetState.hx
@@ -197,7 +197,7 @@ class NoteOffsetState extends MusicBeatState
 		FlxG.sound.playMusic(Paths.music('offsetSong'), 1, true);
 
                 #if android
-                addVirtualPad(FULL, A_B_C);
+                addVirtualPad(FULL, A_B);
                 addPadCamera();
                 #end
 
