87407ece45f5096d989accefe6e4f001a86b571c
Boyfriend-FNF <83258333+Boyfriend-FNF@users.noreply.github.com>
2022-03-08 19:40:20 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -152,7 +152,9 @@ class MainMenuState extends MusicBeatState
 		}
 		#end
 
-	        addVirtualPad(UP_DOWN, A_B);
+                #if android
+                addVirtualPad(UP_DOWN, A_B);
+                #end
 
 		super.create();
 	}
