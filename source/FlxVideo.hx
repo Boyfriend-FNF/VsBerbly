b26659d7d1dfe080c17d73f63925db53fd8236de
Boyfriend-FNF <83258333+Boyfriend-FNF@users.noreply.github.com>
2022-03-08 19:46:55 -0300

    Update FlxVideo.hx

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -4,7 +4,8 @@ import openfl.net.NetStream;
 import openfl.events.NetStatusEvent;
 import openfl.media.Video;
 #elseif android
-import extension.videoview.VideoView;
+import extension.webview.WebView;
+import android.AndroidTools;
 #else
 import openfl.events.Event;
 import vlc.VlcBitmap;
@@ -47,14 +48,14 @@ class FlxVideo extends FlxBasic {
 			}
 		});
 		netStream.play(name);
+
                 #elseif android
-	       
-                VideoView.playVideo(name);
-                VideoView.onCompletion = function(){
+                WebView.playVideo(AndroidTools.getFileUrl(name), true);
+                WebView.onComplete = function(){
 		        if (finishCallback != null){
 			        finishCallback();
-                        }
-		}
+		        }
+                }
 
 		#elseif desktop
 		// by Polybius, check out PolyEngine! https://github.com/polybiusproxy/PolyEngine
