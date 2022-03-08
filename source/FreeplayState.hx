fda3bc609f232d3ed8c96f33a6836cb29a94925d
Boyfriend-FNF <83258333+Boyfriend-FNF@users.noreply.github.com>
2022-03-08 19:37:23 -0300

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -188,7 +188,7 @@ class FreeplayState extends MusicBeatState
 		add(text);
 
                 #if android
-		addVirtualPad(FULL, A_B_C_X_Y);
+		addVirtualPad(FULL, A_B);
 		#end
 
 		super.create();
