3591080be9f80ef556e6ca8a7ad98d6d4cf0150b
Boyfriend-FNF <83258333+Boyfriend-FNF@users.noreply.github.com>
2022-03-10 07:41:41 -0300

    Update NoteOffsetState.hx

--- source/options/NoteOffsetState.hx
+++ source/options/NoteOffsetState.hx
@@ -300,7 +300,7 @@ class NoteOffsetState extends MusicBeatState
 				}
 			}
 
-			if(controls.RESET#if android || _virtualpad.buttonC.justPressed #end)
+			if(controls.RESET)
 			{
 				for (i in 0...ClientPrefs.comboOffset.length)
 				{
