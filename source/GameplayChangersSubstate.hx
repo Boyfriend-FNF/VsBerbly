e9fcc72f4fa1acb9cf1d3c9272332a46077fd618
Boyfriend-FNF <83258333+Boyfriend-FNF@users.noreply.github.com>
2022-03-10 07:38:09 -0300

    Update GameplayChangersSubstate.hx

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -132,6 +132,11 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		changeSelection();
 		reloadCheckboxes();
+
+                #if android
+                addVirtualPad(FULL, A_B_C);
+                addPadCamera();
+                #end
 	}
 
 	var nextAccept:Int = 5;
